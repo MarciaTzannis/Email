b77f79557a58b37c765e4bbd2e7c4b7c34678ea3
Marcia Tzannis <marciatzannis@gmail.com>
2020-01-22 21:57:37 -0300

    formulario de cadastro

--- cmail/src/app/app.routes.ts
+++ cmail/src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from "./modules/cadastro/cadastro.component";
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './modules/login/login.component';
 import { CaixaDeEntradaComponent } from './modules/caixa-de-entrada/caixa-de-entrada.component';
 
 const rotas: Routes = [
-    { path: '', component: LoginComponent },
-    { path: 'inbox', component: CaixaDeEntradaComponent},
-    { path: 'cadastro', component: CadastroComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'inbox', component: CaixaDeEntradaComponent },
+    { path: 'cadastro', component: CadastroComponent },
+    { path: '', pathMatch: 'full', redirectTo: 'inbox' },
+    { path: '**', pathMatch: 'full', redirectTo: 'login' }
 ]
 
 export const ModuleRoteamento = RouterModule.forRoot(rotas);
