e98921fa40f5ecaa2c3f06e58dde410dc95aac04
Marcia Tzannis <marciatzannis@gmail.com>
2020-01-24 22:23:56 -0300

    HTTP Resquest

--- cmail/src/app/modules/cadastro/cadastro.component.ts
+++ cmail/src/app/modules/cadastro/cadastro.component.ts
@@ -1,23 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { conditionallyCreateMapObjectLiteral } from '@angular/compiler/src/render3/view/util';
+import { HttpClient, HttpResponseBase, HttpErrorResponse } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { User } from 'src/app/models/user';
+import { Router } from '@angular/router';
+
 
 @Component({
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
+  mensagemErro = "";
 
   formCadastro = new FormGroup({
-    nome: new FormControl('', [ Validators.required, Validators.minLength(3), Validators.maxLength(6) ]),
-    username: new FormControl('', [ Validators.required, Validators.minLength(6) ]),
-    senha: new FormControl('', [ Validators.required, Validators.pattern('[a-z]*') ]),
-    avatar: new FormControl('')
+    nome: new FormControl('', [ Validators.required, Validators.minLength(3) ]),
+    username: new FormControl('', [ Validators.required ]),
+    senha: new FormControl('', [ Validators.required ]),
+    telefone: new FormControl('', [ Validators.required, Validators.pattern('[0-9]{4}-?[0-9]{4,5}') ]),
+    avatar: new FormControl('', [ Validators.required ], this.validaImagem.bind(this) )
   })
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private roteador: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  validaImagem(campoDoFormulario: FormControl) {
+    return this.httpClient
+      .head(campoDoFormulario.value, {
+        observe: 'response'
+      })
+      .pipe(
+        map((response: HttpResponseBase) => {
+          return response.ok ? null : { urlInvalida: true }
+        }),
+        catchError((error) => {
+          return [{ urlInvalida: true }]
+        })
+      )
   }
 
   validarTodosOsCamposDoFormulario() {
@@ -32,7 +56,26 @@ export class CadastroComponent implements OnInit {
  
   handleCadastroUsuario() {
     if(this.formCadastro.valid) {
-      this.formCadastro.reset();
+      const userData = new User
+      (this.formCadastro.value);
+      console.log(userData);
+
+      this.httpClient
+        .post('http://localhost:3200/users', userData)
+        .subscribe (
+          (resp) => {
+            console.log(resp);
+            this.formCadastro.reset();
+
+            setTimeout(() => {
+              this.roteador.navigate(['']);
+            }, 1000)
+          },
+          (responseError: HttpErrorResponse) => {
+            this.mensagemErro = responseError.error.body;
+          }
+        )
+
     } else {
       this.validarTodosOsCamposDoFormulario();
     }
